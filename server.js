f5a0fe6f456c646882c146216077aadfc8fca4fe
Nikhil_Tony21 <nikhilranga43@gmail.com>
2025-06-28 00:47:58 +0530

    CROS Erros solved

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // CORS setup
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173'||process.env.FRONTEND_URL,
   credentials: true
 }));
 
